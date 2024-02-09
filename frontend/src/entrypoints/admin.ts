570c950ce0f566f687cac2b78c2d2f2162b1c147
Syfaro <syfaro@huefox.com>
2024-02-08 19:05:09 -0500

    Update for bundler changes.

--- frontend/src/entrypoints/admin.ts
+++ frontend/src/entrypoints/admin.ts
@@ -1,5 +1,80 @@
+import * as echarts from "echarts";
+
 document.getElementById("inject-job")?.addEventListener("click", (ev) => {
   if (!confirm("Are you sure you want to inject this job?")) {
     ev.preventDefault();
   }
 });
+
+interface IngestStatUpdate {
+  timestamp: number;
+  counts: Record<string, number>;
+}
+
+let pointDates: Array<Date> = [];
+let series: Record<string, any> = {};
+let previousValues: Record<string, number> = {};
+let addedPoints = -1;
+
+const chart = echarts.init(document.getElementById("ingest-stats"));
+
+const opts = {
+  title: {
+    left: "center",
+    text: "Ingested Media",
+  },
+  tooltip: {
+    trigger: "axis",
+  },
+  toolbox: {
+    feature: {
+      dataZoom: {
+        yAxisIndex: "none",
+      },
+      restore: {},
+      saveAsImage: {},
+    },
+  },
+  xAxis: {
+    type: "category",
+    boundaryGap: false,
+    data: pointDates,
+  },
+  yAxis: {
+    name: "Additions",
+    type: "value",
+    boundaryGap: [0, "100%"],
+  },
+  series: [],
+};
+
+chart.setOption(opts);
+
+const evtSource = new EventSource("/api/ingest/stats");
+
+evtSource.onmessage = (e) => {
+  const data: IngestStatUpdate = JSON.parse(e.data);
+
+  for (let [key, value] of Object.entries(data["counts"])) {
+    if (series[key] === undefined) {
+      series[key] = {
+        name: key,
+        type: "line",
+        data: Array(addedPoints + 1).fill(0),
+      };
+    }
+
+    series[key]["data"].push(value - (previousValues[key] || 0));
+    previousValues[key] = value;
+  }
+
+  pointDates.push(new Date(data["timestamp"] * 1000));
+  addedPoints += 1;
+
+  chart.setOption({
+    xAxis: {
+      data: pointDates,
+    },
+    series: Object.values(series),
+  });
+};
